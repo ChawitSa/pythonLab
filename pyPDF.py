9cd16c8f11ba032d9fd4492bd4e1185cb6642fb7
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-08 07:19:44 +0700

    Update pyPDF.py

--- pyPDF.py
+++ pyPDF.py
@@ -1,5 +1,6 @@
 import PyPDF4
 
+
 def PDFRotation(originalFileName, newFileName, rotateAngle):
         pdfReader = PyPDF4.PdfFileReader(originalFileName)
         pdfWriter = PyPDF4.PdfFileWriter()
@@ -8,11 +9,12 @@ def PDFRotation(originalFileName, newFileName, rotateAngle):
                 pageObject = pdfReader.getPage(page)
                 pageObject.rotateClockwise(rotateAngle)
 
-                pdfwriter.addPage(pageObject)
+                pdfWriter.addPage(pageObject)
         newFile = open(newFileName, "wb")
         pdfWriter.write(newFile)
         newFile.close()
         print("Rotation Done!!")
+
 
 def PDFMerger(pdfs, outputFileName):
         pdfMerger = PyPDF4.PdfFileMerger()
@@ -20,14 +22,68 @@ def PDFMerger(pdfs, outputFileName):
                 pdfMerger.append(pdf)
         newFile = open(outputFileName, "wb")
         pdfMerger.write(newFile)
-        newFile.clone()
+        pdfMerger.close()
         print("Merger Done!!")
 
-originalFileName = "Test.pdf"
+
+def PDFSplitting(originalFileName, splitPosition):
+        start = 0
+        end = splitPosition[0]
+        for i in range(len(splitPosition)+1):
+                pdfReader = PyPDF4.PdfFileReader(originalFileName)
+                pdfWriter = PyPDF4.PdfFileWriter()
+
+                for page in range(start, end):
+                        pdfWriter.addPage(pdfReader.getPage(page))
+                outputpdf = "SplittedFile"+str(i+1)+".pdf"
+
+                newFile = open(outputpdf, "wb")
+                pdfWriter.write(newFile)
+                newFile.close()
+
+                start = end
+
+                try:
+                        end = splitPosition[i+1]
+                except IndexError:
+                        end = pdfReader.getNumPages()
+
+        print("Splitting Done!!")
+
+
+def watermarkMaking(originalFileName, outputFileName, watermarkFileName):
+        pdfReader = PyPDF4.PdfFileReader(originalFileName)
+        watermarkReader = PyPDF4.PdfFileReader(watermarkFileName)
+        mergePDF = PyPDF4.PdfFileWriter()
+
+        watermark = watermarkReader.getPage(0)
+
+        for page in range(pdfReader.getNumPages()):
+                currentPage = pdfReader.getPage(page)
+                currentPage.mergePage(watermark)
+
+                mergePDF.addPage(currentPage)
+
+        newFile = open(outputFileName, "wb")
+        mergePDF.write(newFile)
+        newFile.close()
+        print("Making Waterpark Done!!")
+
+
+originalFileName = "test.pdf"
 newFileName = "Rotated_example.pdf"
 rotateAngle = 270
 PDFRotation(originalFileName, newFileName, rotateAngle)
 
-pdfs = ["Test1.pdf", "Test2.pdf"]
+pdfs = ["test.pdf", "test1.pdf"]
 outputFileName = "Combined_example.pdf"
 PDFMerger(pdfs, outputFileName)
+
+originalFileName = "test.pdf"
+splitPosition = [2, 5]
+PDFSplitting(originalFileName, splitPosition)
+
+originalFileName = "test.pdf"
+outputFileName = "Watermark.pdf"
+watermarkFileName = "logo.pdf"
+watermarkMaking(originalFileName, outputFileName, watermarkFileName)
