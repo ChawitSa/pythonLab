8b31b8cb368db36b9f213d97e8f9691e4e00ca0f
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-09 07:32:59 +0700

    Create mp4Downloader.py

---
+++ mp4Downloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+def youtube_downloader():
+    youtube_object = YouTube(link)
+    youtube_object = youtube_object.streams.get_highest_resolution()
+    try:
+        youtube_object.download()
+    except:
+        print("Some Errors Occurred")
+    print("Done !")
+
+link = input("Enter youtube url: ")
+youtube_downloader()
